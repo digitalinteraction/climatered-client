82140bf97bbeacd43912ab724818e82f86034451
Rob Anderson <rob@andrsn.uk>
2020-07-07 10:10:12 +0100

    use ctx.createScriptProcessor for the prototype

--- src/audio.js
+++ src/audio.js
@@ -6,6 +6,7 @@ export const BroadcastState = {
 export const AUDIO_SAMPLE_RATE = 44100
 export const AUDIO_LOW_LEVEL = 3
 export const AUDIO_HIGH_LEVEL = 32
+export const AUDIO_CHUNK_SIZE = 16 * 1024
 
 //
 // A class for listening to the microphone and sending data to a callback
@@ -37,6 +38,38 @@ export class AudioBroadcaster {
       // sampleRate: AUDIO_SAMPLE_RATE
     })
 
+    //
+    // Get a stream of the user's microphone
+    //
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        advanced: [{ sampleRate: AUDIO_SAMPLE_RATE }]
+      }
+    })
+    this.stream = stream
+
+    // await this.startUsingRecorder()
+    // await this.startUsingAudioWorklet()
+    await this.startUsingScriptProcessor()
+
+    this.state = BroadcastState.active
+  }
+
+  async startUsingRecorder() {
+    const recorder = new MediaRecorder(this.stream, {
+      mimeType: 'audio/webm',
+      audioBitsPerSecond: AUDIO_SAMPLE_RATE
+    })
+
+    recorder.ondataavailable = async event => {
+      this.onData(event.data)
+    }
+
+    recorder.start(1000)
+    this.recorder = recorder
+  }
+
+  async startUsingAudioWorklet() {
     await this.ctx.audioWorklet.addModule('/socket-record-processor.js')
 
     //
@@ -55,36 +88,25 @@ export class AudioBroadcaster {
     this.recorder = recorder
 
     //
-    // Get a stream of the user's microphone
-    //
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: {
-        advanced: [{ sampleRate: AUDIO_SAMPLE_RATE }]
-      }
-    })
-    this.stream = stream
-
-    //
     // Create a source to wrap our stream into the WebAudio API
     //
     const source = this.ctx.createMediaStreamSource(this.stream)
     source.onended = () => this.stop()
     source.connect(recorder)
     this.source = source
+  }
 
-    // const recorder = new MediaRecorder(stream, {
-    //   mimeType: 'audio/webm',
-    //   audioBitsPerSecond: AUDIO_SAMPLE_RATE
-    // })
-    //
-    // recorder.ondataavailable = async event => {
-    //   this.onData(event.data)
-    // }
-    //
-    // recorder.start(1000)
-    // this.recorder = recorder
+  async startUsingScriptProcessor() {
+    const processor = this.ctx.createScriptProcessor(AUDIO_CHUNK_SIZE, 1, 0)
+    processor.onaudioprocess = e => {
+      this.onData(e.inputBuffer.getChannelData(0).buffer)
+    }
+    this.processor = processor
 
-    this.state = BroadcastState.active
+    const source = this.ctx.createMediaStreamSource(this.stream)
+    source.onended = () => this.stop()
+    source.connect(processor)
+    this.source = source
   }
 
   async stop() {
@@ -94,16 +116,29 @@ export class AudioBroadcaster {
 
     this.state = BroadcastState.inactive
     this.stream?.getTracks().forEach(t => t.stop())
-    // this.recorder?.stop()
 
+    // await this.stopUsingWebRecorder()
+    // await this.stopUsingAudioWorklet()
+    await this.stopUsingScriptProcessor()
+
+    await this.ctx.close()
+  }
+
+  async stopUsingWebRecorder() {
+    this.recorder?.stop()
+  }
+
+  async stopUsingAudioWorklet() {
     this.source.disconnect()
 
     this.recorder.port.onmessage = null
     this.recorder.disconnect()
+  }
 
-    this.state = BroadcastState.inactive
-
-    await this.ctx.close()
+  async stopUsingScriptProcessor() {
+    this.source.disconnect()
+    this.source = null
+    this.processor = null
   }
 
   handleStreamError(error) {
@@ -151,6 +186,8 @@ export class AudioReciever {
   }
 
   push(data) {
+    if (this.state === RecieverState.inactive) return
+
     const floats = new Float32Array(data)
     const buffer = this.ctx.createBuffer(
       1,
@@ -220,55 +257,58 @@ export class AudioReciever {
     const canvasWidth = 400
     const canvasHeight = 100
 
-    const segmentSize = 256 * 128
-
     ctx.fillStyle = '#1e2428'
     ctx.fillRect(0, 0, canvasWidth, canvasHeight)
 
-    const totalSamples = segmentSize * this.buffers.length
+    const rawBuffers = this.buffers.map(b => b.getChannelData(0))
+    const totalSamples = rawBuffers.reduce((sum, b) => sum + b.length, 0)
     const samplesPerPixel = Math.floor(totalSamples / canvasWidth)
 
-    const rawBuffers = this.buffers.map(b => b.getChannelData(0))
-
     const getSample = globalIndex => {
-      const segmentIndex = Math.floor(globalIndex / segmentSize)
-      if (segmentIndex > this.buffers.length - 1) {
-        console.log(this.buffers)
-        return 0
+      let i = 0
+
+      for (let buffer of rawBuffers) {
+        if (globalIndex - i >= buffer.length) {
+          i += buffer.length
+        } else {
+          return buffer[globalIndex - i]
+        }
       }
 
-      const segment = rawBuffers[segmentIndex]
-      const localIndex = globalIndex % segmentSize
-      return segment[localIndex]
+      throw new Error('Sample out of index')
     }
 
-    let largest = -1
-    for (let i = 0; i < totalSamples; i += totalSamples / 100) {
-      const sample = Math.abs(getSample(Math.floor(i)))
-      if (sample) largest = sample
-    }
+    const amplify = 2
+    ctx.lineWidth = 2
+    ctx.strokeStyle = '#00c073'
+    ctx.moveTo(0, canvasHeight / 2)
+    ctx.beginPath()
 
+    // Loop across each pixel of the canvas
     for (let x = 0; x < canvasWidth - 1; x++) {
       const min = x * samplesPerPixel
       const max = (x + 1) * samplesPerPixel - 1
 
-      let sum = 0
+      // let sum = 0
+      let largest = -1
+      let smallest = 1
       for (let j = min; j < max; j++) {
-        sum += getSample(j)
+        let sample = getSample(j)
+        if (sample > largest) largest = sample
+        if (sample < smallest) smallest = sample
+        // sum += getSample(j)
       }
 
-      let avg = sum / (max - min)
-
-      // let y = (avg * canvasHeight) / 2
-      // let y = canvasHeight - height
+      // let avg = sum / (max - min)
+      let avg = -smallest > largest ? smallest : largest
 
       // const y = avg * (canvasHeight / 2) + canvasHeight / 2
-      const y = ((avg / largest + 1) / 2) * canvasHeight
+      const y = ((avg * amplify + 1) / 2) * canvasHeight
 
-      // console.log(y)
-
-      ctx.fillStyle = '#00c073'
-      ctx.fillRect(x, y + 1, 1, 3)
+      // ctx.fillStyle = '#00c073'
+      // ctx.fillRect(x - 1, y - 1, 3, 3)
+      ctx.lineTo(x, y)
     }
+    ctx.stroke()
   }
 }
