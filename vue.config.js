e100f57bccc32c0816b3dd585897b1d4ae2b8c82
Rob Anderson <rob@andrsn.uk>
2020-08-14 10:19:38 +0100

    update svg loader config

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,12 @@ module.exports = {
     // Clear the existing svg rule and load as a component instead
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
-    svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
 
     // prettier-ignore
     config.module
