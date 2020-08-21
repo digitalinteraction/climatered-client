c8f3e9fa34fe7cbe287f48b11dd4ec89acc702cd
Fiona Galston <fiona.galston@newcastle.ac.uk>
2020-08-21 15:39:47 +0100

    test data see more panel

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -79,7 +79,42 @@
                 </div>
               </div>
             </div>
-            <p class="content"></p>
+            <div class="content">
+              <p>A simple Read More, Read Less pen in Vue.js</p>
+
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Venenatis lectus magna fringilla urna. Etiam tempor orci eu
+                lobortis. Integer quis auctor elit sed vulputate mi sit. Lacinia
+                at quis risus sed vulputate odio ut enim blandit. Nibh praesent
+                tristique magna sit amet purus. Eleifend donec pretium vulputate
+                sapien nec sagittis. Facilisi morbi tempus iaculis urna id
+                volutpat. Ultrices neque ornare aenean euismod.<span
+                  v-if="readMore"
+                ></span>
+                <span v-else>...</span>
+              </p>
+
+              <p v-show="readMore">
+                Ligula ullamcorper malesuada proin libero nunc consequat
+                interdum varius. Turpis egestas pretium aenean pharetra magna ac
+                placerat. Sed egestas egestas fringilla phasellus faucibus
+                scelerisque eleifend donec. Sed cras ornare arcu dui. Aliquam
+                vestibulum morbi blandit cursus. Adipiscing elit ut aliquam
+                purus sit amet. Aenean sed adipiscing diam donec adipiscing
+                tristique risus nec. Ut etiam sit amet nisl purus in mollis. Eu
+                mi bibendum neque egestas congue quisque egestas diam in.
+                Pellentesque adipiscing commodo elit at imperdiet dui accumsan
+                sit.
+              </p>
+              <div class="see-more">
+                <p class="button is-text" @click="readMore = !readMore">
+                  <span v-if="readMore">See Less</span>
+                  <span v-else>See More</span>
+                </p>
+              </div>
+            </div>
           </div>
           <div class="column is-one-third right-event-panel">
             <div class="info-panel">
@@ -211,7 +246,8 @@ export default {
     return {
       slotState: 'active',
       messages: [],
-      chatMessage: ''
+      chatMessage: '',
+      readMore: false
     }
   },
   mounted() {
@@ -360,5 +396,12 @@ h3 {
 
 .info-panel > *:not(:last-child) {
   margin-bottom: 2em;
+}
+
+.see-more {
+  text-align: center;
+  p {
+    display: inline-block;
+  }
 }
 </style>
