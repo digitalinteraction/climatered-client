12c27dde275da655027d5879876a91c1b3a8ed1e
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2020-09-07 14:21:56 +0100

    Moved all nav logic into component and display triangle on all screens

--- src/components/AppWrapper.vue
+++ src/components/AppWrapper.vue
@@ -2,45 +2,8 @@
   <div class="app-wrapper">
     <div class="app-header">
       <div class="app-header-start">
-        <!--
-            Nav bar
-            .has-shadow
-            .has-border
-        -->
-        <nav
-          class="navbar has-border is-fixed-top"
-          role="navigation"
-          aria-label="main navigation"
-        >
-          <div class="navbar-brand">
-            <!-- Logo -->
-            <router-link :to="atriumRoute" class="navbar-item" active-class="">
-              <img
-                src="/img/brand-new.svg"
-                alt="Home"
-                width="160"
-                height="28"
-              />
-            </router-link>
-
-            <!-- Menu button (burger) -->
-            <a
-              role="button"
-              class="navbar-burger burger"
-              aria-label="menu"
-              aria-expanded="false"
-              data-target="navbarBasicExample"
-              @click.prevent="toggleMenu"
-              ref="menuButton"
-            >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </a>
-          </div>
-          <!-- Menu -->
-          <NavMenu :showing-menu="showingMenu" />
-        </nav>
+        <!-- Nav bar -->
+        <NavBar />
       </div>
     </div>
 
@@ -63,41 +26,25 @@
 // adding nav, tabs and a <slot> for the page
 // and only displays <slot> when data has been fetched
 //
-import NavigationMixin from '@/mixins/NavigationMixin.js'
-
 import { mapState } from 'vuex'
 
 import AppFooter from '@/components/AppFooter.vue'
 
-import NavMenu from '@/components/layout/NavMenu.vue'
+import NavBar from '@/components/layout/NavBar.vue'
 import SideTabs from '@/components/layout/SideTabs.vue'
 
 export default {
-  mixins: [NavigationMixin],
-  components: { AppFooter, NavMenu, SideTabs },
-  data() {
-    return {
-      showingMenu: false
-    }
-  },
+  components: { AppFooter, NavBar, SideTabs },
   computed: {
     ...mapState('api', ['user', 'settings', 'apiState']),
     hasData() {
       return this.apiState === 'active'
-    }
-  },
-  methods: {
-    toggleMenu() {
-      this.showingMenu = !this.showingMenu
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-$tri-height: $navbar-height / 2;
-$tri-width: $tabbar-width / 2;
-
 .app-wrapper {
   position: relative;
   min-height: 100vh;
@@ -116,18 +63,5 @@ $tri-width: $tabbar-width / 2;
 
 .app-header {
   z-index: 50;
-}
-
-.navbar-brand {
-  @include desktop {
-    &:before {
-      content: '';
-      display: inline-block;
-      border-block-end: solid $tri-height $cc-coral;
-      border-inline-start: solid $tri-width $cc-coral;
-      border-inline-end: solid $tri-width $white;
-      border-block-start: solid $tri-height $white;
-    }
-  }
 }
 </style>
