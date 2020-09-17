3301cb1286a7005bced4073a857cd4d28f6ca570
Rob Anderson <robanderson@hey.com>
2020-09-17 10:04:58 +0100

    Add ConferenceOver component over to atrium text

---
+++ src/components/ConferenceOver.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    v-if="conferenceIsOver"
+    class="notification is-success has-text-weight-bold"
+  >
+    {{ $t('atrium.conferenceOver') }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    conferenceIsOver() {
+      return Boolean(this.$store.state.api.settings.conferenceIsOver)
+    }
+  }
+}
+</script>
