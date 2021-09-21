7e22acd4bd984a905b924d8bdd82c5dc880f5154
Rob Anderson <robanderson@hey.com>
2021-09-21 15:05:03 +0100

    fix: add login text

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,15 +1,21 @@
 <template>
   <BrandedUtilLayout>
-    <LoginView api-module="api" />
+    <LoginView api-module="api">
+      <ApiContent slug="login" slot="infoText" />
+      <p slot="doneText">
+        {{ $t('deconf.login.doneText') }}
+      </p>
+    </LoginView>
   </BrandedUtilLayout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import BrandedUtilLayout from '@/components/BrandedUtilLayout.vue'
+import ApiContent from '@/components/ApiContent.vue'
 import { LoginView } from '@openlab/deconf-ui-toolkit'
 
 export default Vue.extend({
-  components: { BrandedUtilLayout, LoginView },
+  components: { BrandedUtilLayout, LoginView, ApiContent },
 })
 </script>
