31cf7bac2b9db7cdb0030cc94683ee2c13e51394
Rob Anderson <rob.anderson@ncl.ac.uk>
2021-09-29 14:16:06 +0100

    fix(help): simplify and secure landbot by using an iframe directly

--- src/views/main/HelpDeskView.vue
+++ src/views/main/HelpDeskView.vue
@@ -1,6 +1,12 @@
 <template>
   <IfrcAppLayout>
-    <div class="helpdesk" ref="root"></div>
+    <div class="helpdesk">
+      <iframe
+        class="helpdesk-frame"
+        :src="landbotSource"
+        frameborder="0"
+      ></iframe>
+    </div>
   </IfrcAppLayout>
 </template>
 
@@ -8,96 +14,20 @@
 import Vue from 'vue'
 import IfrcAppLayout from '@/components/IfrcAppLayout.vue'
 
-const SCRIPT_SRC = 'https://static.landbot.io/landbot-3/landbot-3.0.0.js'
-
 const CHAT_SOURCES = {
-  en: 'https://chats.landbot.io/v3/H-696982-BAZDZ3L80F9LBJS0/index.json',
-  fr: 'https://chats.landbot.io/v3/H-700110-7X6TH2ZJ8EZAB2X4/index.json',
-  es: 'https://chats.landbot.io/v3/H-700112-RPM4983E6BWMSCLH/index.json',
-  ar: 'https://chats.landbot.io/v3/H-700114-DUJHTAN0MSA0GMLE/index.json',
+  en: 'https://chats.landbot.io/v3/H-696982-BAZDZ3L80F9LBJS0/index.html',
+  fr: 'https://chats.landbot.io/v3/H-700110-7X6TH2ZJ8EZAB2X4/index.html',
+  es: 'https://chats.landbot.io/v3/H-700112-RPM4983E6BWMSCLH/index.html',
+  ar: 'https://chats.landbot.io/v3/H-700114-DUJHTAN0MSA0GMLE/index.html',
 }
 
 type Locale = 'en' | 'fr' | 'es' | 'ar'
-interface LandbotContainer {
-  destroy(): void
-}
-interface LandbotWindow {
-  Landbot?: {
-    Container: {
-      new (options: unknown): LandbotContainer
-    }
-  }
-}
-
-interface Data {
-  frameInstace: LandbotContainer | null
-}
-
-const landbotWindow = window as LandbotWindow
 
 export default Vue.extend({
   components: { IfrcAppLayout },
-  data(): Data {
-    return {
-      frameInstace: null,
-    }
-  },
   computed: {
     landbotSource(): string {
       return CHAT_SOURCES[this.$i18n.locale as Locale]
-    },
-  },
-  watch: {
-    landbotSource(newValue) {
-      this.setBotSource(newValue)
-    },
-  },
-  mounted() {
-    this.setup()
-  },
-  methods: {
-    async setup() {
-      await this.checkForScript()
-      this.setBotSource(this.landbotSource)
-    },
-    async checkForScript() {
-      // Do nothing if already loaded
-      if (landbotWindow.Landbot) return
-
-      // Create the script tag
-      const script = document.createElement('script')
-      script.setAttribute('type', 'text/javascript')
-      script.setAttribute('SameSite', 'None; Secure')
-      script.setAttribute('async', 'true')
-      script.setAttribute('src', SCRIPT_SRC)
-
-      // Append the script to the DOM
-      document.body.appendChild(script)
-
-      // Wait for it to be loaded
-      return new Promise((resolve) => {
-        script.addEventListener('load', () => {
-          resolve(script)
-        })
-      })
-    },
-    setBotSource(source: string) {
-      const rootRef = this.$refs.root as Element
-      if (!this.$refs.root || !landbotWindow.Landbot) return
-
-      // If there is already a frame, destroy it
-      if (this.frameInstace) this.frameInstace.destroy()
-
-      // Create a frame for the bot to go in (destroy will remove it)
-      const div = document.createElement('div')
-      div.setAttribute('id', 'landbotFrame')
-      rootRef.appendChild(div)
-
-      // Create and store the frame
-      this.frameInstace = new landbotWindow.Landbot.Container({
-        container: '#landbotFrame',
-        configUrl: source,
-      })
     },
   },
 })
@@ -109,7 +39,7 @@ export default Vue.extend({
   background: $background;
   position: relative;
 }
-#landbotFrame {
+.helpdesk-frame {
   position: absolute;
   width: 100%;
   height: 100%;
